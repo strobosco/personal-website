00b0ec636b617b9bc786d6918ef0029043f263f7
strobosco <54740672+strobosco@users.noreply.github.com>
2021-09-24 18:26:47 -0400

    Added NavBar

---
+++ components/NavBar.tsx
@@ -0,0 +1,76 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/system/Box";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+import React from "react";
+
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const NavBar: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Portfolio for Alexander Grattan, software developer studying Digital Narrative and Interactive Design at the University of Pittsburgh. Skilled with React, Gatsby, Next.js, JavaScript, TypeScript, Java, Python, TailwindCSS, Sass, Node.js, Django, and GSAP."
+        ></meta>
+        <title>Niccolo Nobili</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Box sx={{ flexGrow: 1, height: 20 }}>
+        <AppBar position="static" sx={{ height: 80, bgcolor: "#212121" }}>
+          <Toolbar>
+            <Typography sx={{ lineHeight: 8, ml: 20, fontSize: 38 }}>
+              Niccolo Nobili
+            </Typography>
+            <Box sx={{ position: "absolute", right: 0, mr: 20 }}>
+              <a
+                href="https://github.com/strobosco"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  sx={{ mr: 10 }}
+                >
+                  <FontAwesomeIcon icon={faGithub} size="1x"></FontAwesomeIcon>
+                </IconButton>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/niccolonobili/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  sx={{ mr: 2 }}
+                >
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    size="1x"
+                  ></FontAwesomeIcon>
+                </IconButton>
+              </a>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default NavBar;
