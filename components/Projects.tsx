f7ac557b2cc5832b9dd68cb9ee88aaa288689243
strobosco <54740672+strobosco@users.noreply.github.com>
2021-09-26 14:41:51 -0400

    Updated descriptions

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,7 +11,7 @@ import {
 } from "@mui/material";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Projects: NextPage = () => {
   return (
