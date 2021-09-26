40918b8262422bbf53068dab9f9225bf4f45cb0c
strobosco <54740672+strobosco@users.noreply.github.com>
2021-09-26 13:52:11 -0400

    Added projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 import type { NextPage } from "next";
 import NavBar from "../components/NavBar";
+import AboutMe from "../components/AboutMe";
+import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <AboutMe />
+      <Projects />
+    </>
+  );
 };
 
 export default Home;
