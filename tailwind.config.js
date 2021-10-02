42df693aa0cf83d631f032eac7c2456399634016
strobosco <54740672+strobosco@users.noreply.github.com>
2021-10-02 16:41:49 -0400

    Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    backgroundColor: (theme) => ({
+      primary: "#212121",
+      card: "#1b1b1b",
+    }),
+    fontSize: {
+      heading: "46px",
+      footer: "40px",
+      icon: "46px",
+      footerIcon: "60px",
+      p: "20px",
+    },
+    fontFamily: {
+      body: '"Gill Sans"',
+    },
+    borderRadius: {
+      DEFAULT: "5px",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
