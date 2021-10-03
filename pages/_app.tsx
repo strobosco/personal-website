0d3113e9a472c2f4a6e11746e025fb3d439d028b
strobosco <54740672+strobosco@users.noreply.github.com>
2021-10-03 14:31:40 -0400

    Overwrite MUI styles?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 
 import { StylesProvider } from "@material-ui/core/styles";
+import { createTheme } from "@mui/material/styles";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = createTheme({});
+
   return (
     <StylesProvider injectFirst={true} disableGeneration={true}>
       <Component {...pageProps} />
