b27f85026b9ef823c7f45621f3358e24ee74efaa
strobosco <54740672+strobosco@users.noreply.github.com>
2022-01-13 09:45:40 -0500

    added link to resume

--- components/Footer.js
+++ components/Footer.js
@@ -6,16 +6,28 @@ const Footer = () => {
     <div className="section link-div">
       <p className="footer-text">Connect with me!</p>
       <div className="footer-links">
-        <a href="https://linkedin.com/in/niccolonobili">
+        <a
+          href="https://linkedin.com/in/niccolonobili"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           <FaLinkedin />
         </a>
-        <a href="https://github.com/strobosco">
+        <a
+          href="https://github.com/strobosco"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           <FaGithub />
         </a>
-        <a href="mailto:nobilicareers@gmail.com">
+        <a
+          href="mailto:nobilicareers@gmail.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           <AiOutlineMail />
         </a>
-        <a>
+        <a href="./resume.pdf" rel="noopener noreferrer" target="_blank">
           <p>Resume</p>
         </a>
       </div>
